e0076f943a3be3e83890e0b3aa033e029c4b54d6
taskie <t@skie.jp>
2015-05-11 12:58:25 +0000

    initial commit

---
+++ run_gitit.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -eu
+
+(
+    cd wikidata
+    git pull
+    git checkout develop
+)
+
+PATH=.cabal-sandbox/bin/:"$PATH"
+exec nohup gitit -f ./gitit.conf > stdout.log 2> stderr.log &
