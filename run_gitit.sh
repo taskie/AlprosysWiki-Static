9548b1c6cbf6806b55888ced5c3bf6ac863cafdf
taskie <t@skie.jp>
2017-02-19 22:39:32 +0900

    use M+ 2p Latin of www.alprosys.com

--- run_gitit.sh
+++ run_gitit.sh
@@ -6,4 +6,11 @@ set -eu
     git pull
 )
 
-exec nohup gitit -f ./gitit.conf >> stdout.log 2>> stderr.log &
+if [ -f pid-file ]; then
+    kill -TERM $(cat pid-file)
+    rm pid-file
+    sleep 3
+fi
+
+nohup gitit -f ./gitit.conf >> stdout.log 2>> stderr.log &
+echo $! > pid-file
